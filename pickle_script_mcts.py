00fb726ac0e967e52af7b0eda0edf098d9389898
Kavinda Senewiratne <senewiry@oregonstate.edu>
2022-01-26 11:09:30 -0800

    refractored

--- pickle_script_mcts.py
+++ pickle_script_mcts.py
@@ -21,81 +21,43 @@ if __name__ == "__main__":
     debug_reward_network_list = pickle.load(infile)
     infile.close()
 
-    abs_max1 = np.amax(np.abs(debug_reward_greedy_list))
-    print('abs_max1', abs_max1)
-    normalized_array_greedy = np.asarray(debug_reward_greedy_list) * (5.0 / abs_max1)
-
-    abs_max2 = np.amax(np.abs(debug_reward_network_list))
-    print(abs_max2)
-    normalized_array_network = np.asarray(debug_reward_network_list) * (5.0 / abs_max2)
-
-    print('debug_reward_greedy_list', len(normalized_array_greedy))
-    print('debug_reward_network_list', normalized_array_network)
-
+    # create axes for scatter plot
     y1 = debug_reward_greedy_list
     print("debug_reward_greedy_list: ", len(y1))
-    x1 = np.array([i for i in range(len(y1))])
 
     y2 = debug_reward_network_list
     print("debug_reward_network_list", len(y2))
-    x2 = np.array([i for i in range(len(y2))])
-
-    # This was actually a wrong plot, but can also show the convergence (use box plot below instead)
-    # plt.scatter(x1[90000:100000], y1[90000:100000], s=2.0, label='greedy')
-    # plt.scatter(x2[90000:100000], y2[90000:100000], s=2.0, label="network")
-    # plt.legend(loc='best')
-    # plt.show()
 
     # This is for seeing whether greedy and network are linear
     # Network should be linear with greedy because the network is trained mostly on greedy data
     # The 90-100k is there because we want to check only for a single trial
-    # plt.scatter(y1[90000:100000], y2[90000:100000], s=2.0)
-    plt.scatter(y1, y2, s=2.0)
-    plt.xlabel("greedy")
-    plt.ylabel("network")
-    plt.show()
-
-    # plt.scatter(y1[0:10000], y2[0:10000], s=2.0)
-    # plt.scatter(y1[10000:20000], y2[10000:20000], s=2.0)
-    # plt.scatter(y1[20000:30000], y2[20000:30000], s=2.0)
+    # plt.scatter(y1, y2, s=2.0)
     # plt.xlabel("greedy")
     # plt.ylabel("network")
     # plt.show()
 
-    
-    increment = 10000
-    # data_1 = normalized_array_network[0:10000]
-    # data_2 = normalized_array_network[0+increment:10000+increment]
-    # data_3 = normalized_array_network[0+(increment*2):10000+(increment*2)]
-    # data_4 = normalized_array_network[0+(increment*3):10000+(increment*3)]
-    # data_5 = normalized_array_network[0+(increment*4):10000+(increment*4)]
-    # data_6 = normalized_array_network[0+(increment*5):10000+(increment*5)]
-    # data_7 = normalized_array_network[0+(increment*6):10000+(increment*6)]
-    # data_8 = normalized_array_network[0+(increment*7):10000+(increment*7)]
-    # data_9 = normalized_array_network[0+(increment*8):10000+(increment*8)]
-    # data_10 = normalized_array_network[0+(increment*9):10000+(increment*9)]
-    # data = [data_1, data_2, data_3, data_4, data_5, data_6, data_7, data_8, data_9, data_10]
-    # data = [data_10]
-
-    increment2 = 1000
-    data_1 = normalized_array_network[(increment*9):(increment*9)+(increment2)]
-    data_2 = normalized_array_network[(increment*9):(increment*9)+(increment2*2)]
-    data_3 = normalized_array_network[(increment*9):(increment*9)+(increment2*3)]
-    data_4 = normalized_array_network[(increment*9):(increment*9)+(increment2*4)]
-    data_5 = normalized_array_network[(increment*9):(increment*9)+(increment2*5)]
-    data_6 = normalized_array_network[(increment*9):(increment*9)+(increment2*6)]
-    data_7 = normalized_array_network[(increment*9):(increment*9)+(increment2*7)]
-    data_8 = normalized_array_network[(increment*9):(increment*9)+(increment2*8)]
-    data_9 = normalized_array_network[(increment*9):(increment*9)+(increment2*9)]
-    data_10 = normalized_array_network[(increment*9):(increment*9)+(increment2*10)]
-    data = [data_1, data_2, data_3, data_4, data_5, data_6, data_7, data_8, data_9, data_10]
+    # see if reward is converging
+    # the for loop separates the data from a single trial into diff segments so we can boxplot
+    # the increment was chose by seeing the length of the list and splitting it evenly somehow
+    increment = len(debug_reward_greedy_list)//12
+    start = 0
+    end = increment
+    data_dict = {}
+    for i in range(12):
+        data_dict["data_" + str(i+1)] = debug_reward_greedy_list[start:end]
+        start = end
+        end += increment
 
     fig = plt.figure(figsize =(10, 7))
     
     # Creating axes instance
     ax = fig.add_axes([0, 0, 1, 1])
     
-    # Creating plot
+    data = list()
+    for data_list in data_dict.values():
+        data.append(data_list)
+
+    # create box plot 
     bp = ax.boxplot(data)
     
     # show plot
