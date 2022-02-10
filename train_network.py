e3e33d3477c58c1ce8bd4ee9c56115bd66193155
Graeme Best <bestg@oregonstate.edu>
2022-02-09 17:30:22 -0800

    refractored

--- train_network.py
+++ train_network.py
@@ -2,17 +2,17 @@ import NeuralNet
 import pickle
 import torch
 import numpy as np
+from util import get_random_loc, oracle_visualize, communicate, get_CONF, get_json_comp_conf
+
 
 if __name__ == "__main__":
 
+    CONF = get_CONF()
+    json_comp_conf = get_json_comp_conf()
+
     # unpickle all the data
     print("Unpickling started!")
-    # alienware
-    # filename = '/home/kavi/thesis/pickles/data_21x21_circles_random_greedyo_r4_t1000_s50_norollout_diffstartloc'
-    # filename = '/home/kavi/thesis/pickles/data_21x21_circles_random_greedyo_r4_t1000_s50_rollout_diffstartloc'
-    filename = '/home/kavi/thesis/pickles/data_21x21_circles_random_greedyno_r4_t2000_s25_rollout_diffstartloc'
-    # macbook
-    # filename = '/Users/kavisen/osu_thesis/data/data_21x21_circles_random_greedyno_r4_t800_s50_rollout'
+    filename = CONF[json_comp_conf]["pickle_path"] + "data_21x21_circles_random_greedyno_r4_t2500_s25_rolloutotherpath_samestartloc"
     infile = open(filename,'rb')
     data = pickle.load(infile)
     infile.close()
@@ -20,12 +20,7 @@ if __name__ == "__main__":
 
     # this is the path where the NN weights will be saved
     
-    # alienware
-    # weights_path = "/home/kavi/thesis/neural_net_weights/circles_21x21_epoch4_random_greedyo_r4_t1000_s50_norollout_diffstartloc"
-    # weights_path = "/home/kavi/thesis/neural_net_weights/circles_21x21_epoch1_random_greedyo_r4_t2000_s25_rollout_diffstartloc"
-    weights_path = "/home/kavi/thesis/neural_net_weights/test"
-    # macbookcircles_random_greedyo_r4_t2000_s25_rollout_diffstartloc
-    # weights_path = "/Users/kavisen/osu_thesis/neural_net_weights/circles_21x21_epoch5_random_greedyno_r4_t800_s50_rollout"
+    weights_path = CONF[json_comp_conf]["neural_net_weights_path"] + "circles_21x21_epoch1_random_greedyno_r4_t2500_s25_rolloutotherpath_samestartloc"
 
     # train network - initial 
     bounds = [21, 21]
