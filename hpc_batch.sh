005cd859b313bda80fd6f7061bfac5e5a01f837e
Kavinda Senewiratne <senewiry@oregonstate.edu>
2022-06-13 13:03:24 -0700

    updated

--- hpc_batch.sh
+++ hpc_batch.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
-SBATCH -J eval 06/13 12:41
-SBATCH -A mime
-#SBATCH -o logs/%x-%j.out
-#SBATCH -e logs/%x-%j.err
-#SBATCH -t 7-00:00:00
-SBATCH --mail-type=BEGIN,END,FAIL
-SBATCH --mail-user=senewiry@oregonstate.edu
+#SBATCH -J eval 06/13 12:41
+#SBATCH -A mime
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=senewiry@oregonstate.edu
 #SBATCH -p dgx
 #SBATCH -n 1 # request tasks
 #SBATCH -c 8 # request 8 cores
-SBATCH -N 2 # number of nodes
-SBATCH --gres=gpu:1 # request a GPU
+#SBATCH -N 2 # number of nodes
+#SBATCH --gres=gpu:1 # request a GPU
 #SBATCH --constraint=v100                  # request node with V100 GPU
 #SBATCH --mem=20GB # request X GB
 #SBATCH --nodelist=dgx2-5
 
 # Load any required software environment module
-. ~/python/miniconda3/etc/profile.d/conda.sh
+/nfs/stak/users/senewiry/miniconda3/etc/profile.d/conda.sh
+conda init bash
 conda activate thesis
 
 # run my job
