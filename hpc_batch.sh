00535af06a89a5928aa5e2f60a095444ad72252a
Kavinda Senewiratne <senewiry@submit-a.hpc.engr.oregonstate.edu>
2022-06-18 15:43:11 -0700

    works with osu hpc cluster

--- hpc_batch.sh
+++ hpc_batch.sh
@@ -5,16 +5,15 @@
 #SBATCH --mail-user=senewiry@oregonstate.edu
 #SBATCH -p dgx
 #SBATCH -n 1 # request tasks
-#SBATCH -c 4 # request 8 cores
-#SBATCH -N 2 # number of nodes
+#SBATCH -c 2 # request 2 cores
 #SBATCH --gres=gpu:1 # request a GPU
 #SBATCH --constraint=v100                  # request node with V100 GPU
 #SBATCH --mem=20GB # request X GB
-#SBATCH --nodelist=dgx2-5
+#SBATCH -o /nfs/stak/users/senewiry/osu_thesis/outfiles/log.11.out
 
 # Load any required software environment module
 . ~/miniconda3/etc/profile.d/conda.sh
 conda activate thesis
 
 # run my job
-python3 /nfs/stak/users/senewiry/osu_thesis/main.py eval 1
+python3 /nfs/stak/users/senewiry/osu_thesis/main.py eval 11
