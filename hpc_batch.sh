28f19d9a17f01717cfa5782595d1602910a244a6
Kavinda Senewiratne <senewiry@submit-a.hpc.engr.oregonstate.edu>
2022-06-23 17:09:10 -0700

    SCRIPT_NO variable added

--- hpc_batch.sh
+++ hpc_batch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 #SBATCH -J eval
 #SBATCH -A mime
 #SBATCH -t 7-00:00:00
@@ -9,11 +10,11 @@
 #SBATCH -c 2 # request 2 cores
 #SBATCH --gres=gpu:1 # request a GPU
 #SBATCH --mem=20GB # request X GB
-#SBATCH -o /nfs/stak/users/senewiry/osu_thesis/outfiles/log.11.out
+#SBATCH -o /nfs/stak/users/senewiry/osu_thesis/outfiles/log.out
 
 # Load any required software environment module
 . ~/miniconda3/etc/profile.d/conda.sh
 conda activate thesis
 
 # run my job
-python3 /nfs/stak/users/senewiry/osu_thesis/main.py eval 11
+python3 /nfs/stak/users/senewiry/osu_thesis/main.py eval $SCRIPT_NO
